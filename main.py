47dedff1f13c05011726db3222a7b3c80b95ed48
Batman <aqibakku345@gmail.com>
2024-09-15 21:07:54 +0530

    New Update for main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#Import Section
+# Import Section
 from sys import path
 path.insert(0,"./module/")
 
 import calc
 from tkinter import *
-from functools import partial
 
-#Variables
+# Variables
 Dictionary = {"+":calc.Add,"-":calc.Diff,"*":calc.Product,"/":calc.Div}
 Buttons_num = dict()
 Buttons_Oprs = dict()
@@ -17,33 +16,33 @@ Window_Size = "400x400"
 Window_Title = "Calculator"
 
 # Functions
-def validate_only_digits(Key):
-    return Key.isdigit() or Key == ''
-
-def validate_only_opers(Key):
-    return True if Key in ['+','-','/','*',''] else False
+def Validate(Key):
+    if len(Key) == 0:
+        return True
+    if Key[-1].isdigit():
+        return True
+    elif Key[-1] in ['+', '-', '/', '*', '']:
+        if Key[-2] not in ['+', '-', '/', '*']:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 def Calc():
     Output_Area.config(state=NORMAL)
     Output_Area.delete('1.0', END)
-    Operator = "+" if Input_Area_0.get() == '' else Input_Area_0.get()
-    A = 0 if not Input_Area_1.get().isdigit() else int(Input_Area_1.get())
-    B = 0 if not Input_Area_2.get().isdigit() else int(Input_Area_2.get())
-    Output = str(Dictionary[Operator](A,B))
+    Raw_Input = Input_Area.get()
+    if Raw_Input == "":
+        Output = "None"
+    else:
+        Input = Raw_Input[1::] if Raw_Input[0] == '0' else Raw_Input
+        Output = str(eval(Input))
     Output_Area.insert(END, Output)
     Output_Area.config(state=DISABLED)
 
 def Place(Val):
-    try:
-        Focus_Input = window.focus_get()
-        Focus_Input.insert(END,Val)
-    except Exception as Error:
-        Focus_Input = Input_Area_1
-        Focus_Input.insert(END, Val)
-
-def Operator_Place(Opr):
-    Input_Area_0.delete(0, END)
-    Input_Area_0.insert(END, Opr)
+    Input_Area.insert(END, Val)
 
 def Create_Num_Buttons():
     tmp_x = 50
@@ -70,40 +69,44 @@ def Create_Oprs_Buttons():
     count = 0
     for o in Oprs:
         count += 1
-        Buttons_Oprs[o] = Button(window, text=f"{o}", command=lambda o=o:Operator_Place(f"{o}"))
+        Buttons_Oprs[o] = Button(window, text=f"{o}", command=lambda o=o:Place(f"{o}"))
         Buttons_Oprs[o].place(x=tmp_x,y=tmp_y,width=button_width,height=button_height)
         tmp_x += 70
         if count % 2 == 0:
             tmp_y += 50
             tmp_x = 230
-# Main
+
+def Clear_All():
+    Input_Area.delete(0, END)
+    
+    Output_Area.config(state=NORMAL)
+    Output_Area.delete('1.0', END)
+    Output_Area.config(state=DISABLED)
+
+# Window and Widgets
 window = Tk()
 window.geometry(Window_Size)
 window.title(Window_Title)
 window.resizable(False, False)
 
-Input_Area_1 = Entry(window, validate='key', validatecommand=(window.register(validate_only_digits), '%P'))
-Input_Area_1.insert(END, "0")
-Input_Area_1.place(x=50, y=50,width=100, height=30)
-
-Input_Area_0 = Entry(window, validate='key', validatecommand=(window.register(validate_only_opers), '%P'))
-Input_Area_0.insert(END, "+")
-Input_Area_0.place(x=170, y=50, width=30, height=30)
-
-Input_Area_2 = Entry(window, validate='key', validatecommand=(window.register(validate_only_digits), '%P'))
-Input_Area_2.insert(END, "0")
-Input_Area_2.place(x=220, y=50,width=100, height=30)
+Input_Area = Entry(window, validate='key', validatecommand=(window.register(Validate), '%P'))
+Input_Area.insert(END, "0")
+Input_Area.place(x=50, y=50, width=280, height=30)
 
 Output_Area = Text(window, wrap='word', width=300, height=30)
-Output_Area.insert(END, "0")
 Output_Area.config(state=DISABLED)
 Output_Area.place(x=50, y=100, width=280, height=30)
 
 Calculate = Button(window,text="Calc", command=Calc)
 Calculate.place(x=230, y=150, width=100, height=40)
 
+Clear = Button(window, text='CA', command=Clear_All)
+Clear.place(x=230, y=300,width=50, height=40)
+
 window.bind("<Return>", lambda event: Calc())
 
 Create_Num_Buttons()
 Create_Oprs_Buttons()
+
+# mainloop
 window.mainloop()
